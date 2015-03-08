b11f84e30d5a8288f52f02bfbb410dd555f1f266
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2015-03-08 21:30:48 +0000

    Changes so far: (1)Created TICTACTOE object with only two methods publically available - game and game10; all other variables and functions are private and can't be accessed; (2) changed majority of functions into function expressions; (3)changed the way tie was identified - previous code would sometimes score tie before all fields of the board were filled; (4) moved some repeated code into functions;

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,95 +1,13 @@
 // TIC TAC TOE Battle
 
 // The main action of the Tic Tac Toe game runs in this function
-function game() {
-	//TODO create a board object that holds functions like update board, and create board.
-	var board = createBoard(); // init TTT board 
-	var plr1 = new player("1"); // player 1 is 'X'
-	var plr2 = new playerRand("2"); // player 2 is 'O'
-	var isPlr1 = choseRand(); // choose start player
-	var nextMove;
-	var gameOver = -1;
 
-	console.log(printBoard(board));
-
-	for(var i = 1; i <= 9; i++) { //there are only 9 tiles.
-		if(isPlr1) {
-			nextMove = plr1.move(board);
-		}else{
-			nextMove = plr2.move(board);
-		}
-
-		if(isValidMove(nextMove, board)) {
-		    //Player 1 goes
-			if(isPlr1) {
-				board[nextMove[0]][nextMove[1]] = "X";
-				printMove();
-				gameOver = isGameWon(board, "X");
-				if (gameOver === 1) {
-					setInnerHTMLById("ans", getInnerHTMLById("ans") + "<hr>" + printBoardHTML(board, nextMove));
-					setInnerHTMLById("ans", getInnerHTMLById("ans") + "<h2>PLAYER 1 WINS THE GAME!</h2>");
-					return;
-				}else if (gameOver === 0) {
-				    document.getElementById("ans").innerHTML += "<hr>" + printBoardHTML(board, nextMove);
-                    document.getElementById("ans").innerHTML += "<h2>NO ONE WINS. ITS A TIE!</h2>";
-                    document.getElementById("tie").innerHTML += "I";
-                    console.log(printBoard(board));
-                    return;
-				}else{
-				    //for good measure
-				}
-			//Player 2 goes
-			}else{
-				board[nextMove[0]][nextMove[1]] = "O";
-				printMove();
-				gameOver = isGameWon(board, "O");
-                if (gameOver === 1) {
-                	setInnerHTMLById("ans", getInnerHTMLById("ans") + "<hr>" + printBoardHTML(board, nextMove));
-					setInnerHTMLById("ans", getInnerHTMLById("ans") + "<h2>PLAYER 2 WINS THE GAME!</h2>");
-					return;
-				}else if (gameOver === 0) {
-                     setInnerHTMLById("ans", getInnerHTMLById("ans") + "<hr>" + printBoardHTML(board, nextMove));
-		     setInnerHTMLById("ans", getInnerHTMLById("ans") + "<h2>NO ONE WINS. ITS A TIE!</h2>");
-                     document.getElementById("tie").innerHTML += "I";
-                     return;
-                 }else{
-                     //for good measure
-                 }
-			}
-		}else{
-			if(isPlr1){
-				setInnerHTMLById("ans", getInnerHTMLById("ans") + "<hr>" + printBoardHTML(board, nextMove));
-				setInnerHTMLById("ans", getInnerHTMLById("ans") + "Invalid move from Player 1<br><h2>PLAYER 2 WINS THE GAME BECAUSE OF FORFAIT</h2>");
-				break;
-			}else{
-				setInnerHTMLById("ans", getInnerHTMLById("ans") + "<hr>" + printBoardHTML(board, nextMove));
-				setInnerHTMLById("ans", getInnerHTMLById("ans") + "Invalid move from Player 2<br><h2>PLAYER 1 WINS THE GAME BECAUSE OF FORFAIT</h2>");
-				break;
-			}
-			break;// not useful..
-		}
-
-		isPlr1 = !isPlr1;
-	}
-	return;
-
-	function printMove(){
-		console.log(printBoard(board));
-        document.getElementById("ans").innerHTML += "<hr>" + printBoardHTML(board);
-	}
-}
-
-function game10() {
-	for(var i = 0; i < 10; i++) {
-		game();
-	}
-}
-
+var TICTACTOE = (function(){
 
 // created the empty Tic Tac Toe board
-function createBoard() {
+var createBoard = function() {
 	return [["-","-","-"], ["-","-","-"], ["-","-","-"]];
-}
+};
 
 // Finds the 1st available space and plays 
 function player(plr) {
@@ -106,7 +24,7 @@ function player(plr) {
 			}
 		}
 	};
-}
+};
 
 // Randomly finds an empty space and plays there
 function playerRand(plr) {
@@ -125,10 +43,10 @@ function playerRand(plr) {
 	function rand() {
 		return Math.floor(Math.random()*3);
 	}
-}
+};
 
 // gives a 50-50 change for a player to start 1st
-function choseRand() {
+var choseRand = function() {
 	var x = Math.random();
 
 	if(x >= 0.5) {
@@ -136,19 +54,19 @@ function choseRand() {
 	}else{
 		return false;
 	}
-}
+};
 
 //determines whether the move is valid. If the space is empty.
-function isValidMove(move, board) {
+var isValidMove = function(move, board) {
 	if(board[move[0]][move[1]] === "-") {
 		return true;
 	}else{
 		return false;
 	}
-}
+};
 
 //Determines if there is a winner
-function isWinner(board) {
+var isWinner = function(board) {
 	var b = board; // for clarity
 	//check diagonal
 	if(b[0][0] != "-" && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
@@ -170,16 +88,16 @@ function isWinner(board) {
 		}
 		return false;
 	}
-}
+};
  
 // changes an empty space to the most recent move.
-function updateBoard(board,nextMove, isPlr1) {
+var updateBoard = function(board,nextMove, isPlr1) {
 	if(isPlr1) {
 		board[nextMove[0]][nextMove[1]] = "X";
 	}else{
 		board[nextMove[0]][nextMove[1]] = "O";
 	}
-}
+};
 
 
 /*
@@ -192,7 +110,7 @@ Prints the board is the shape below if a string format. For testing.
 
 */
 
-function printBoard(b) {
+var printBoard = function(b) {
 	var text = "";
 	var col = "\n---+---+---\n";
 	var row = " |";
@@ -207,10 +125,10 @@ function printBoard(b) {
 		if(c < 2) text += col;
 	}
 	return text;
-}
+};
 
 //Prints the board for visualization on the HTML page
-function printBoardHTML(b) {
+var printBoardHTML = function(b) {
 	var text = "";
 	var col = "<br>---+---+---<br>";
 	var row = "&nbsp|";
@@ -225,61 +143,60 @@ function printBoardHTML(b) {
 		if(c < 2) text += col;
 	}
 	return text;
-}
+};
 
 //Determines if there is a winner 
 //+1 win, 0 tie, -1 lose
-function isGameWon(board, type) {
+var isGameWon = function(board, type) {
 	var loopCounter = 0;
 	var rowTotal = 0;
 	var columnTotal = 0;
 	var diagonal1Total = 0;
 	var diagonal2Total = 0;
+	var takenSpacesTotal = 0;
 	var WIN = 1;
 	var TIE = 0;
-	var NOTOVER = 1;
-	var isEmpty = true;
+	var NOTOVER = 2;
+
 
 	// counter for columns
 	for (var c=0; c<3; c++){
 		//counter for rows
 		for (var r=0; r<3; r++){
 			loopCounter++;
+			//check if the cell is already occupied
+			if (board[c][r] !== '-'){
+				takenSpacesTotal++;
+			}
 			// test rows
 			if(board[c][r] === type){
-				rowTotal++;
-				if (rowTotal === 3) {
-					return WIN;
-				}
-			}
-			// test columns
-			if (board[r][c] === type){
-				columnTotal++;
-				if (columnTotal === 3) {
-					return WIN;
-				}
-			}
-			// test diagonals
-			if (r === c) {
-				if (board[r][c] === type){
-					diagonal1Total++;
-					if (diagonal1Total === 3) {
-						return WIN;
-					}
-				}
-				if (board[c][2-c] === type){
-					diagonal2Total++;
-					if (diagonal2Total === 3) {
-						return WIN;
-					}
-				}
-			}
-			
-			if (board[c][r] === '-') {
-			    isEmpty = true;
-			}else{
-			    isEmpty = false;
-			}
+            	rowTotal++;
+           		if (rowTotal === 3) {
+            		return WIN;
+            	}
+            }
+            // test columns
+            if (board[r][c] === type){
+            	columnTotal++;
+            	if (columnTotal === 3) {
+            		return WIN;
+            	}
+            }
+            // test diagonals
+            if (r === c) {
+            	if (board[r][c] === type){
+            		diagonal1Total++;
+            		if (diagonal1Total === 3) {
+            			return WIN;
+            		}
+            	}
+            	if (board[c][2-c] === type){
+            		diagonal2Total++;
+            		if (diagonal2Total === 3) {
+            			return WIN;
+            		}
+            	}
+            }
 
 			// reset column and row counters
 			if (loopCounter%3 === 0) {
@@ -289,22 +206,115 @@ function isGameWon(board, type) {
 		}
 	}
 	//check if the board is full but there are no winners
-	if(isEmpty === false) {
+	if(takenSpacesTotal === 9) {
 	    return TIE;
 	}
+
 	return NOTOVER;
-}
+};
 
 
 //Helper function
 //sets the inner HTML of a given id to text
-function setInnerHTMLById(id, text) {
+var setInnerHTMLById = function(id, text) {
 	document.getElementById(id).innerHTML = text;
-}
+};
 
 //helper function
 //gets the innerHTML of a given id
-function getInnerHTMLById(id) {
+var getInnerHTMLById = function(id) {
 	return document.getElementById(id).innerHTML;
-}
+};
 
+//make only game and game10 public
+return {
+	game: function() {
+    	//TODO create a board object that holds functions like update board, and create board.
+    	var board = createBoard(); // init TTT board
+    	var plr1 = new player("1"); // player 1 is 'X'
+    	var plr2 = new playerRand("2"); // player 2 is 'O'
+    	var isPlr1 = choseRand(); // choose start player
+    	var nextMove;
+    	var gameOver = -1;
+
+    	//console.log(printBoard(board));
+
+    	for(var i = 1; i <= 9; i++) { //there are only 9 tiles.
+    		if(isPlr1) {
+    			nextMove = plr1.move(board);
+    		}else{
+    			nextMove = plr2.move(board);
+    		}
+
+    		if(isValidMove(nextMove, board)) {
+    		    //Player 1 goes
+    			if(isPlr1) {
+    				board[nextMove[0]][nextMove[1]] = "X";
+    				printMove();
+    				gameOver = isGameWon(board, "X");
+    				if (gameOver === 1 ) {
+    					updateWinCounter(1,"plr1");
+    					return;
+    				}else if (gameOver === 0) {
+                        updateWinCounter();
+                        return;
+    				}else{
+    				    //for good measure
+    				}
+    			//Player 2 goes
+    			}else{
+    				board[nextMove[0]][nextMove[1]] = "O";
+    				printMove();
+    				gameOver = isGameWon(board, "O");
+                    if (gameOver === 1) {
+    					updateWinCounter(2,"plr2");
+    					return;
+    				}else if (gameOver === 0) {
+    		     		updateWinCounter();
+                        return;
+                     }else{
+                         //for good measure
+                     }
+    			}
+    		}else{
+    			if(isPlr1){
+    				invalidMove(1);
+    				break;
+    			}else{
+    				invalidMove(2);
+    				break;
+    			}
+    		}
+
+    		isPlr1 = !isPlr1;
+    	}
+    	return;
+
+    	function printMove(){
+            document.getElementById("ans").innerHTML += "<hr>" + printBoardHTML(board);
+    	}
+
+    	function updateWinCounter(player, id){
+    		if (id){
+				setInnerHTMLById("ans", getInnerHTMLById("ans") + "<h2>PLAYER "+player+" WINS THE GAME!</h2>");
+				document.getElementById(id).innerHTML += "I";
+            } else {
+            	setInnerHTMLById("ans", getInnerHTMLById("ans") + "<h2>NO ONE WINS. ITS A TIE!</h2>");
+                document.getElementById("tie").innerHTML += "I";
+            }
+    	}
+    	function invalidMove(player){
+    		setInnerHTMLById("ans", getInnerHTMLById("ans") + "<hr>" + printBoardHTML(board, nextMove));
+            setInnerHTMLById("ans", getInnerHTMLById("ans") + "Invalid move from Player "+player+"<br><h2>OPPONENT WINS THE GAME BECAUSE OF FORFAIT</h2>");
+    	}
+    },
+
+    game10: function() {
+    		for(var i = 0; i < 10; i++) {
+    			this.game();
+    		}
+    	}
+
+};
+
+})();
